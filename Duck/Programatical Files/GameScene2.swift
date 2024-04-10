2a32846c1ccf6bb8ac51e7110013ba23fccef210
bfddeveloper <bdicksondev@gmail.com>
2024-04-10 10:01:17 -0500

    dukcs work again + back button

--- Duck/Programatical Files/GameScene2.swift
+++ Duck/Programatical Files/GameScene2.swift
@@ -1,31 +1,24 @@
-//
-//  GameScene2.swift
-//  Duck
-//
-//  Created by Josh Madison on 3/21/24.
-//
-
 import SpriteKit
-import GameplayKit //Havent fully figured out gameplay kit but always import no matter what to coincide with SpriteKit
 
-class GameScene2: SKScene { //SKScene is how we are seeing any of the stuff on screen, see buttons for custom classes
+class GameScene2: SKScene {
     
-    var ogDuck = SKSpriteNode()
-    var FireDuck = SKSpriteNode()
-    var EarthDuck = SKSpriteNode()
     var currentIndex = 0
-    var mySprites: [SKSpriteNode] = []
+    var duckSprites: [SKSpriteNode] = []
+    var ogDuck: SKSpriteNode!
+    var fireDuck: SKSpriteNode!
+    var earthDuck: SKSpriteNode!
+    var selectionArrowLeft: SKSpriteNode!
+    var selectionArrowRight: SKSpriteNode!
 
-    override func didMove(to view: SKView) { //Runs when the certain screen is opened, creating background, buttons, and labels.
+    override func didMove(to view: SKView) {
         createBackground()
         createButtons()
         createLabels()
         let backgroundSound = SKAudioNode(fileNamed: "QuackCharacterSelect")
         self.addChild(backgroundSound)
-
     }
     
-    func createBackground() { //Originally supposed to be a moving background but changed so loop does not matter
+    func createBackground() {
         let river = SKTexture(imageNamed: "duckgamebackground")
         for i in 0...1 {
             let riverBackground = SKSpriteNode(texture: river)
@@ -35,97 +28,88 @@ class GameScene2: SKScene { //SKScene is how we are seeing any of the stuff on s
         }
     }
     
-    func createLabels() { //Function created by me to run in didMove()
-        
-        let logo = SKTexture(imageNamed: "ChooseYourDucks") //private variables can be established by any SKNode(SKSpriteNode, SKTexture, etc.)
+    func createLabels() {
+        let logo = SKTexture(imageNamed: "ChooseYourDucks")
         let chooseADuck = SKSpriteNode(texture: logo)
-        chooseADuck.position = CGPoint(x: frame.midX, y: frame.maxY - 300) //Once established, vars use modifiers kind of in the same way as .modifier
-        chooseADuck.size = CGSize(width: 115, height: 115)
+        chooseADuck.position = CGPoint(x: frame.midX, y: frame.maxY - 300)
+        chooseADuck.size = CGSize(width: 130, height: 130) // Increase size
         chooseADuck.zPosition = -2
-        addChild(chooseADuck) //actually adds the item to the scene
-        let scaleBackward = SKAction.scale(to: 0.25, duration: 0.2) //These are extra movement variables to make it have that bouncy effect
+        addChild(chooseADuck)
+        let scaleBackward = SKAction.scale(to: 0.25, duration: 0.2)
         let scaleForward = SKAction.scale(to: 6, duration: 0.3)
         let scaleBackward2 = SKAction.scale(to: 5, duration: 0.2)
         let scaleSequence = SKAction.sequence([scaleForward, scaleBackward2])
         chooseADuck.run(scaleSequence)
         
-        let OGDuck = SKTexture(imageNamed: "Original") //Work in progress due to arrays
-        let fireDuck = SKTexture(imageNamed: "Fire")
-        let earthDuck = SKTexture(imageNamed: "Earth")
-        EarthDuck = SKSpriteNode(texture: OGDuck, size: CGSize(width: 60, height: 40))
-        ogDuck = SKSpriteNode(texture: OGDuck, size: CGSize(width: 60, height: 40))
-        FireDuck = SKSpriteNode(texture: fireDuck, size: CGSize(width: 60, height: 40))
-        ogDuck.position = CGPoint(x: frame.midX, y: frame.midY - 60)
-        ogDuck.size = CGSize(width: 50.0, height: 50.0)
-        addChild(ogDuck) //actually adds the item to the scene
-        ogDuck.run(scaleSequence)
+        ogDuck = createDuck(texture: SKTexture(imageNamed: "Original"), position: CGPoint(x: frame.midX, y: frame.midY))
+        fireDuck = createDuck(texture: SKTexture(imageNamed: "Fire"), position: CGPoint(x: frame.midX, y: frame.midY))
+        earthDuck = createDuck(texture: SKTexture(imageNamed: "Earth"), position: CGPoint(x: frame.midX, y: frame.midY))
+        
+        duckSprites = [ogDuck, fireDuck, earthDuck]
     }
     
-    func createButtons() { //Function created by me to run in didMove()
-        //Might look confusing at first, but split up to differentiate between items keeping code organized
-        
+    func createDuck(texture: SKTexture, position: CGPoint) -> SKSpriteNode {
+        let duck = SKSpriteNode(texture: texture, size: CGSize(width: 60, height: 40))
+        duck.position = position
+        duck.size = CGSize(width: 250.0, height: 250.0)
+        addChild(duck)
+        return duck
+    }
+    
+    func createButtons() {
         let normalTexture = SKTexture(imageNamed: "backButton")
-        let selectedTexture = SKTexture(imageNamed: "backButtonSelected")
-        let customButton = CustomBackButton(defaultTexture: normalTexture, selectedTexture: selectedTexture, size: CGSize(width: 250, height: 250))
-        //refer to custom button files for more
-        customButton.position = CGPoint(x: frame.midX - 200, y: frame.midY + 900)
-        customButton.zPosition = -1
-        addChild(customButton) //actually adds the item to the scene
-        let moveDown = SKAction.moveTo(y: frame.midY + 575, duration: 0.5)
-        customButton.run(moveDown)
+                let selectedTexture = SKTexture(imageNamed: "backButtonSelected")
+                let customButton = CustomBackButton(defaultTexture: normalTexture, selectedTexture: selectedTexture, size: CGSize(width: 250, height: 250))
+                customButton.position = CGPoint(x: frame.midX - 200, y: frame.midY + 900)
+                customButton.zPosition = -1
+                addChild(customButton)
+                let moveDown = SKAction.moveTo(y: frame.midY + 575, duration: 0.5)
+                customButton.run(moveDown)
+        
+        
+        let arrowSize = CGSize(width: 300, height: 300) // Increase size
         
         let arrowLeft = SKTexture(imageNamed: "selectionArrow")
-        let selectionArrowLeft = SKSpriteNode(texture: arrowLeft)
+        selectionArrowLeft = SKSpriteNode(texture: arrowLeft)
         selectionArrowLeft.name = "selectionArrowLeft"
-        selectionArrowLeft.position = CGPoint(x: frame.midX - 250, y: frame.midY - 60)
-        selectionArrowLeft.size = CGSize(width: 200, height: 200)
-        addChild(selectionArrowLeft) //actually adds the item to the scene
+        selectionArrowLeft.position = CGPoint(x: frame.midX - 150, y: frame.midY)
+        selectionArrowLeft.size = arrowSize
+        addChild(selectionArrowLeft)
         
         let arrowRight = SKTexture(imageNamed: "selectionArrow")
-        let selectionArrowRight = SKSpriteNode(texture: arrowRight)
+        selectionArrowRight = SKSpriteNode(texture: arrowRight)
         selectionArrowRight.name = "selectionArrowRight"
-        selectionArrowRight.position = CGPoint(x: frame.midX + 250, y: frame.midY - 60)
-        selectionArrowRight.size = CGSize(width: 200, height: 200)
+        selectionArrowRight.position = CGPoint(x: frame.midX + 150, y: frame.midY)
+        selectionArrowRight.size = arrowSize
         selectionArrowRight.xScale = -1
-        addChild(selectionArrowRight) //actually adds the item to the scene
+        addChild(selectionArrowRight)
         
-        let selectTexture = SKTexture(imageNamed: "SelectButton")
-        let selectedButtonTexture = SKTexture(imageNamed: "SelectButtonSelected")
-        let selectButton = SelectButton(defaultTexture: selectTexture, selectedTexture: selectedButtonTexture, size: CGSize(width: 250, height: 250))
-        selectButton.position = CGPoint(x: frame.midX, y: frame.midY - 300)
-        selectButton.zPosition = -1
-        addChild(selectButton) //actually adds the item to the scene
+        
     }
     
-    func createDucks() { //Function created by me to run in didMove() (not done yet)
-
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { //touchesBegan is a function that handles touch interaction
-        for touch in touches { //essentially a loop to always be looking for touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
             let location = touch.location(in: self)
             let touchedNode = atPoint(location)
             if touchedNode.name == "selectionArrowRight" {
-                currentIndex += 1
-                print("+1")
-                for currentIndex in 0..<mySprites.count {
-                    let currentSprite = mySprites[currentIndex]
-                    ogDuck = currentSprite
-                }
-            }
-            else if touchedNode.name == "selectionArrowLeft" {
-                currentIndex += -1
-                print("-1")
-                for currentIndex in 0..<mySprites.count {
-                    let currentSprite = mySprites[currentIndex]
-                    
-                }
+                currentIndex = (currentIndex + 1) % duckSprites.count
+                updateDisplayedDuck()
+            } else if touchedNode.name == "selectionArrowLeft" {
+                currentIndex = (currentIndex - 1 + duckSprites.count) % duckSprites.count
+                updateDisplayedDuck()
             }
         }
     }
     
+    func updateDisplayedDuck() {
+        for duck in duckSprites {
+            duck.isHidden = true
+        }
+        duckSprites[currentIndex].isHidden = false
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-
+        // Update code here
     }
 }
 
